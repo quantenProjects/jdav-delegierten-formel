8640a81d7f3887030b13bac950e989880a33fc25
quantenProjects <quantenProjects@users.noreply.github.com>
2022-03-19 19:11:16 +0100

    generate_random_data restore random sektion number

--- tools/generate_random_data.py
+++ tools/generate_random_data.py
@@ -4,5 +4,5 @@ import random
 
 if __name__ == '__main__':
     print("Sektion,LV,M,JL")
-    for i in range(1000000): #range(random.randint(1, 1000)):
+    for i in range(random.randint(1, 1000)):
         print(f"Sektion-{i},Mond,{random.randint(0, 2000)},{random.randint(0, 100)}")
